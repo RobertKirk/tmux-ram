96dcd618ae9d3ea585b860fba567821c9b897f02
RobertKirk <robert.kirk.3.14@gmail.com>
2019-02-19 14:30:21 +0000

    further adjustments, still broken

--- scripts/ram_amount.sh
+++ scripts/ram_amount.sh
@@ -4,28 +4,27 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$CURRENT_DIR/helpers.sh"
 
-get_ram_readable() {
+print_ram_amount() {
   ramused=0
   base=''
   humanRAM=''
-  ramused=$(echo $(($(raw_ram_total) - $(raw_ram_available))))
+  size=`echo $(($(raw_ram_total) - $(raw_ram_available)))`
   declare -a suffixes=('B' 'K' 'M' 'G' 'T')
-  size=$ramused
-  remainder=""
+  remainder=''
   index=1
   while [ $(($size/102)) -gt 1 ]; do
-    remainder=".$(( $size % 1024))"
-    size=$(( size/1024 ))
-    index=$(( index + 1 ))
+    remainder=`.$(( $size % 1024))`
+    size=$(( $size / 1024 ))
+    index=$(( $index + 1 ))
   done
 
-  suffix="${suffixes[$index]}"
-  cut_remainder=$(printf "%0.2f" $remainder | cut -c 2-)
+  suffix=`${suffixes[$index]}`
+  cut_remainder=`printf "%0.2f" $remainder | cut -c 2-`
   # echo "$size$cut_remainder$suffix"
   iostat -c 1 2 | sed '/^\s*$/d' | tail -n 1 | awk '{usage=100-$NF} END {printf("%5.1f%%", usage)}' | sed 's/,/./'
 }
 
 main() {
-  get_ram_readable
+  print_ram_amount
 }
 main
